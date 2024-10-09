a2a2424bb4a94de41be0c8193bf57b3b538739bd
Ani <a.gvin3@gmail.com>
2024-10-09 19:03:38 +0400

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Introduction from "./components/introduction";
 import Feature from "./components/Feature";
 import Clients from "./components/clients";
 
